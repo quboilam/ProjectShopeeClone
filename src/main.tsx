0c9ce5c7f93da8325f243d423ea431b442990ac7
BoiLam <tinyfromvietnam@gmail.com>
2023-09-16 22:46:59 +0700

    Cấu hình Axios và TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from 'src/App'
 import './index.css'
-import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
