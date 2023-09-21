fe505a07d19e33246a6daa60d9324f4c0aeaa0e2
BoiLam <tinyfromvietnam@gmail.com>
2023-09-22 01:12:13 +0700

    Hoàn thiện Authentication module

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppProvider } from './contexts/app.context'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,12 +14,13 @@ const queryClient = new QueryClient({
     }
   }
 })
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AppProvider>
+          <App />
+        </AppProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,29 +1,69 @@
-import { useRoutes } from 'react-router-dom'
-import ProductList from './pages/ProductList'
+import { useContext } from 'react'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import { AppContext } from './contexts/app.context'
+import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
 import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Profile from './pages/Profile'
 import Register from './pages/Register'
+
+function ProtectedRoute() {
+  const { isAuthenticated } = useContext(AppContext)
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  const { isAuthenticated } = useContext(AppContext)
+
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
-      element: <ProductList />
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
+      path: '',
+      index: true,
       element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
+        <MainLayout>
+          <ProductList />
+        </MainLayout>
       )
     }
   ])
